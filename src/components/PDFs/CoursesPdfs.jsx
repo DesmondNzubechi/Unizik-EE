eb372750684eaafca4279c1ea6dcaa932a477d14
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-11 02:03:11 +0100

    new change

--- src/components/PDFs/CoursesPdfs.jsx
+++ src/components/PDFs/CoursesPdfs.jsx
@@ -1,17 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { fullNewsContext } from "../context/Context";
-import { allPdfs } from "./PDFs";
+//import { allPdfs } from "./PDFs";
 import {MdAllInbox, MdOutlineFileDownload} from  'react-icons/md';
 import { FaFileDownload } from 'react-icons/fa';
 import { HiDocumentDownload } from 'react-icons/hi';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
 export const DownloadPdfs = () => {
    // const {allPdfs} = useContext(fullNewsContext);
     const [getLevelPdf, setGetLevelPdf] = useState([]);
     const {clickedCoursePdf} = useContext(fullNewsContext);
-    const [currentPdf, setCurrentPdf] = useState([]);
+  const [currentPdf, setCurrentPdf] = useState([]);
+  const [allPdfs, setAllPdfs] = useState([]);
+  const [bookType, setBookType] = useState({
+    Handouts: [],
+    TextBooks: [],
+    pastQuestions: [],
+  })
 
-      useEffect(() => {
+  useEffect(() => {
+        
+    const fetchPdf = async () => {
+      const pdfStore = collection(db, 'learningResources');
+      try {
+        const pdfS = await getDocs(pdfStore);
+        const allPdfData = pdfS.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+        setAllPdfs(allPdfData);
+
+      } catch (error) {
+        alert(error)
+      }
+    }
+    fetchPdf();
         const filterClickedCourse = () => {
           const coursePdf = allPdfs.filter(pdf => {
             return pdf.course === clickedCoursePdf;
@@ -19,7 +40,7 @@ export const DownloadPdfs = () => {
           setCurrentPdf(coursePdf);
         };
         filterClickedCourse();
-      }, [])
+      }, [allPdfs])
     
       console.log(clickedCoursePdf); 
     return(
@@ -35,49 +56,27 @@ export const DownloadPdfs = () => {
           <button className="shadow-2xl p-2 md:text-[15px] text-[12px] rounded bg-slate-100 capitalize font-semibold">Past Question</button>
         </div>
 
-        <div className="grid grid-cols-1  my-[50px] gap-5 md:grid-cols-2 lg:grid-cols-4">
-          <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
-            <div className="flex flex-col gap-2">
-              <h1 className="md:text-[17x] text-[14px] font-bold  ">Flip Flop</h1>  
-              <p className="md:text-[15px] text-[12px]">20mb</p>
-            </div>
-            <HiDocumentDownload className="text-[35px] hover:text-green-500  text-slate-900"/>
-          </div>
-          <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
-            <div className="flex flex-col gap-2">
-              <h1 className="md:text-[17x] text-[14px] font-bold  ">Flip Flop</h1>  
-              <p className="md:text-[15px] text-[12px]">20mb</p>
-            </div>
-            <HiDocumentDownload className="text-[35px] hover:text-green-500  text-slate-900"/>
-          </div>
-          <div className="flex flex-row shadow p-2  hover:bg-slate-100 gap-2 justify-between p-2 border items-center rounded ">
-            <div className="flex flex-col gap-2">
-              <h1 className="md:text-[17x] text-[14px] font-bold  ">Flip Flop</h1>  
-              <p className="md:text-[15px] text-[12px]">20mb</p>
-            </div>
-            <HiDocumentDownload className="text-[35px]  hover:text-green-500  text-slate-900"/>
-          </div>
-          <div className="flex flex-row shadow p-2  hover:bg-slate-100 gap-2 justify-between p-2 border items-center rounded ">
-            <div className="flex flex-col gap-2">
-              <h1 className="md:text-[17x] text-[14px] font-bold  ">Flip Flop</h1>  
-              <p className="md:text-[15px] text-[12px]">20mb</p>
-            </div>
-            <HiDocumentDownload className="text-[35px]  hover:text-green-500  text-slate-900"/>
-          </div>
-        </div>
+       
       
              {currentPdf.length == 0 &&  <div className="flex items-center justify-center"> <h1 className="text-center my-[50px] font-semibold">{clickedCoursePdf}  e-book is currently unavailable. Please Check back later🙏</h1></div> }
-       <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-5 grid-cols-1 ">
+       <div className="grid md:grid-cols-3 my-[50px] lg:grid-cols-4 gap-5 grid-cols-1 ">
       
      {
         currentPdf.map(pdf => {
-            return  <div className="md:max-w-[300px] border hover:border-slate-900 border-slate-300 relative shadow-2xl rounded-[5px] p-[5px]  ">
+          return   <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
+          <div className="flex flex-col gap-2">
+            <h1 className="md:text-[17x] text-[14px] font-bold  ">{pdf.topic}</h1>  
+              <p className="md:text-[15px] text-[12px]">{ pdf.size}</p>
+            </div>
+            <a download={pdf.link} href={pdf.link}> <HiDocumentDownload className="text-[35px] cursor-pointer hover:text-green-500  text-slate-900"/></a>
+        </div>
+          {/* <div className="md:max-w-[300px] border hover:border-slate-900 border-slate-300 relative shadow-2xl rounded-[5px] p-[5px]  ">
             <img src={pdf.pdfImg} className="  " alt="" />
             <span className="flex rounded-b p-[10px] absolute bottom-0 left-0 right-0 flex-row bg-slate-900 justify-between items-center">
                 <h1 className="text-[20px] text-slate-50 ">{pdf.topic}</h1>
               <a href={pdf.downloadLink}  ><MdOutlineFileDownload  className="text-[20px] hover:text-yellow-500 text-slate-50 "/></a>   
             </span>
-          </div> 
+        </div>*/ }
         })
       }
       
--- src/components/PDFs/PDFs.js
+++ src/components/PDFs/PDFs.js
@@ -1,42 +1,5 @@
-/*import { ECE321pdfs } from "../Books/level300Book/FirstSemesterBook/Ece321Book/Ece321book";
-import { ECE323pdfs } from "../Books/level300Book/FirstSemesterBook/Ece323Book/Ece323book";
-import { ECE331pdfs } from "../Books/level300Book/FirstSemesterBook/Ece331Book/Ece331";
-import { ECE333pdfs } from "../Books/level300Book/FirstSemesterBook/Ece333Book/Ece333book";
-import { ELE311pdfs } from "../Books/level300Book/FirstSemesterBook/Ele311Book/Ele311book";
-import { ELE341pdfs } from "../Books/level300Book/FirstSemesterBook/Ele341Book/Ele341book";
-import { ELE343pdfs } from "../Books/level300Book/FirstSemesterBook/Ele343Book/Ele343book";
-import { ELE353pdfs } from "../Books/level300Book/FirstSemesterBook/Ele353Book/Ele353book";
-import { FEG303pdfs } from "../Books/level300Book/FirstSemesterBook/Feg303Book/Feg303book";
 
-import { ECE322pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEce322/Ece322Book";
-import { ECE326pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEce326/Ece326Book";
-import { ECE334pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEce334/Ece334Book";
-
-import { ELE312pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEle312/Ele312Book";
-import { ELE314pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEle314/Ele314Book";
-import { ELE342pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEle342/Ele342Book";
-//import { ELE344pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEle344/Ele344book";
-import { ELE372pdfs } from "../Books/level300Book/SecondSemesterBooks/BookEle372/Ele327Book";
-*/
 
 export const allPdfs = [
-  /*  ...ECE321pdfs,
-    ...ECE323pdfs,
-    ...ECE331pdfs,
-    ...ECE333pdfs,
-    ...ELE311pdfs,
-    ...ELE353pdfs,
-    ...ELE341pdfs,
-    ...ELE343pdfs,
-    ...FEG303pdfs, 
-
-    ...ECE322pdfs,
-    ...ECE326pdfs,
-    ...ECE334pdfs,
-    ...ELE312pdfs,
-    ...ELE314pdfs,
-    ...ELE342pdfs,
-    //...ELE344pdfs,
-    ...ELE372pdfs,*/
-    
+  
 ]
