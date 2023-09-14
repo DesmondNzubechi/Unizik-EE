46c0d1465d8963be223f9855c9fabaeadf1af977
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-14 22:31:23 +0100

    new change

--- src/components/Admin/AllPdf/AllPdf.jsx
+++ src/components/Admin/AllPdf/AllPdf.jsx
@@ -5,20 +5,25 @@ import { fullNewsContext } from "../../context/Context";
 import {MdAllInbox, MdOutlineFileDownload} from  'react-icons/md';
 import { FaFileDownload } from 'react-icons/fa';
 import { HiDocumentDownload } from 'react-icons/hi';
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../config/firebase";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import { db, storage } from "../../config/firebase";
 import { CoursesOffered } from "../../CourseOffered/CourseOffered";
 import { AiFillDelete } from 'react-icons/ai';
+import { deleteObject, ref } from "firebase/storage";
+import { ClipLoader, FadeLoader, MoonLoader, RotateLoader } from "react-spinners";
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export const AllPdf = () => {
+  const [spinC, setSpinC] = useState(false);
     const [courseName, setCourseName] = useState("");
 const [level, setLevel] = useState([]);
 const [getSemester, setGetSemester] = useState('');
 const [selectedCourse, setSelectedCourse] = useState('select');
 const [selectedUnit, setSelectedUnit] = useState(null);
     const [courses, setCourses] = useState([]);
-    const [pdfDatas, setpdfDatas] = useState(null)
+   // const [pdfDatas, setpdfDatas] = useState(null)
 const [pdfDetails, setPdfDetails] =  useState({
       level: '',
       semester: '',
@@ -27,7 +32,7 @@ const [pdfDetails, setPdfDetails] =  useState({
     resourcesType: ''
       
 });
-  // const {allPdfs} = useContext(fullNewsContext);
+  const {allPdfs} = useContext(fullNewsContext);
  // const bookCategories = ['Handouts', 'Textbooks', 'Past Questions'];
   const [bookCat, setBookCat] = useState(JSON.parse(localStorage.getItem('bookCat')) || {
     handoutText: 'text-slate-50',
@@ -43,29 +48,29 @@ const [pdfDetails, setPdfDetails] =  useState({
     const [getLevelPdf, setGetLevelPdf] = useState([]);
     const {clickedCoursePdf} = useContext(fullNewsContext);
   const [currentPdf, setCurrentPdf] = useState([]);
-  const [allPdfs, setAllPdfs] = useState([]);
+ // const [allPdfs, setAllPdfs] = useState([]);
   const [bookType, setBookType] = useState({
     Handouts: [],
     TextBooks: [],
     pastQuestions: [],
   })
   useEffect(() => {
-    // localStorage.setItem('level', json.stringify(level));
-    // localStorage.setItem('selectedCourse', selectedCourse));
-    // localStorage.setItem('getSemester', getSemester);
-    const fetchPdf = async () => {
-      const pdfStore = collection(db, 'learningResources');
-      try {
-        const pdfS = await getDocs(pdfStore);
-        const allPdfData = pdfS.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-        setAllPdfs(allPdfData);
-
-      } catch (error) {
-        alert(error)
-      }
-    }
-    fetchPdf();
     localStorage.setItem('bookCat', JSON.stringify(bookCat))
+    // const fetchPdf = async () => {
+    //   const pdfStore = collection(db, 'learningResources');
+    //   try {
+    //     const pdfS = await getDocs(pdfStore);
+    //     const allPdfData = pdfS.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    //     setAllPdfs(allPdfData);
+
+    //   } catch (error) {
+    //     alert(error)
+    //   }
+    // }
+    // if (allPdfs.length == 0) {
+    //   fetchPdf();
+    //}
+   
         const filterClickedCourse = () => {
           const coursePdf = allPdfs.filter(pdf => {
             return pdf.course === selectedCourse;
@@ -90,11 +95,40 @@ const [pdfDetails, setPdfDetails] =  useState({
       })
     }
     filterBookType();
-      }, [selectedCourse])
+  }, [])
+  
+  const deleteBook = async (pdfInfo) => {
+    const confirmFirst = window.confirm(`Are you want to delete ${pdfInfo.topic}?`);
+    if (!confirmFirst) {
+      return;
+    }
+    setSpinC(true);
+    try {
+      const pdfStore = doc(db, 'learningResources', pdfInfo.id);
+      deleteDoc(pdfStore);
+     const pdfPath = pdfInfo.link.split('/');
+      const pdfToBeDeleted = pdfPath[pdfPath.length - 1];
+      const pdfName = pdfToBeDeleted.split('?')[0];
+      console.log(pdfName.replace('learningResources%2F', ''));
+
+      const deletingPdf = ref(storage, `learningResources/${pdfName.replace('learningResources%2F', '')}`);
+      await deleteObject(deletingPdf);
+      setSpinC(false);
+      const notify = () => toast(`${pdfInfo.topic} successfully deleted`);
+      notify();
+
+    } catch (error) {
+      alert(error);
+    }
+  }
     
-      console.log(clickedCoursePdf); 
+      console.log('all pdfs', allPdfs); 
     return(
-        <div className="pt-[50px] px-[30px]  pb-[50px] ">
+      <div className="pt-[50px] px-[30px]  pb-[50px] ">
+          {spinC && <div className="fixed bg-tpr w-full z-[500] left-0 right-0 flex justify-center h-full top-0 bottom-0 items-center"><RotateLoader className="relative z-[600]" color="#36d7b7"
+           size={30}
+           width={10}
+            /></div> }
         <div className="text-center my-[20px] ">
                 <h1 className="font-bold uppercase text-slate-900 text-[20px] md:text-[30px] "> All Books</h1>
                 <div className=" flex  flex-row justify-center    gap-1 ">
@@ -215,10 +249,6 @@ const [pdfDetails, setPdfDetails] =  useState({
             })
           }} className={`shadow-2xl p-2 md:text-[15px] text-[12px] rounded ${bookCat.pastQuestionBg}  ${bookCat.pastQuestionText} capitalize font-semibold`}>Past Question</button>
         </div>
-
-       
-      
-        
        {   bookType.Handouts.length == 0 && bookCat.handoutState && <h1 className="md:text-[15px] text-[12px] font-bold text-center capitalize mt-[50px] ">{selectedCourse}  handouts is not available now. </h1>  }
        {   bookType.TextBooks.length == 0 && bookCat.textBookState  && <h1 className="md:text-[15px] text-[12px] font-bold text-center capitalize mt-[50px] ">{selectedCourse}  textbook is not available now. </h1>  }
         {  bookType.pastQuestions.length == 0 && bookCat.pastQuestion  && <h1 className="md:text-[15px] text-[12px] font-bold text-center capitalize mt-[50px] ">{selectedCourse}  past exam questions is not available now. </h1>}  
@@ -226,13 +256,13 @@ const [pdfDetails, setPdfDetails] =  useState({
        <div className="grid md:grid-cols-3 my-[50px] lg:grid-cols-4 gap-5 grid-cols-1 ">
       
           {
-        bookCat.handoutState &&   bookType.Handouts.map(pdf => {
-              return <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
+        bookCat.handoutState &&   bookType.Handouts.map((pdf, index) => {
+              return <div key={index} className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
                 <div className="flex flex-col gap-2">
                   <h1 className="md:text-[17x] text-[14px] font-bold  ">{pdf.topic}</h1>
                   <p className="md:text-[15px] text-[12px]">{pdf.size}</p>
                 </div>
-                <a download={pdf.link} href={pdf.link}> <AiFillDelete className="text-[35px] cursor-pointer hover:text-green-500  text-slate-900" /></a>
+                <a onClick={() => deleteBook(pdf)}> <AiFillDelete className="text-[35px] cursor-pointer hover:text-green-500  text-slate-900" /></a>
               </div>
              }
             )
@@ -240,26 +270,26 @@ const [pdfDetails, setPdfDetails] =  useState({
           
           {
             
-         bookCat.textBookState &&    bookType.TextBooks.map(pdf => {
-              return <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
+         bookCat.textBookState &&    bookType.TextBooks.map((pdf, index) => {
+          return <div key={index} className="flex flex-row shadow  p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
                 <div className="flex flex-col gap-2">
                   <h1 className="md:text-[17x] text-[14px] font-bold  ">{pdf.topic}</h1>
                   <p className="md:text-[15px] text-[12px]">{pdf.size}</p>
                 </div>
-                <a download={pdf.link} href={pdf.link}> <AiFillDelete className="text-[35px] cursor-pointer hover:text-green-500  text-slate-900" /></a>
+                <a onClick={() => deleteBook(pdf)}> <AiFillDelete className="text-[35px] cursor-pointer hover:text-green-500  text-slate-900" /></a>
               </div>
              }
             )
           }
           {
             
-           bookCat.pastQuestion &&  bookType.pastQuestions.map(pdf => {
-              return <div className="flex flex-row shadow p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
+           bookCat.pastQuestion &&  bookType.pastQuestions.map((pdf, index) => {
+            return <div key={index} className="flex flex-row shadow  p-2  hover:bg-slate-100  gap-2 justify-between p-2 border items-center rounded ">
                 <div className="flex flex-col gap-2">
                   <h1 className="md:text-[17x] text-[14px] font-bold  ">{pdf.topic}</h1>
                   <p className="md:text-[15px] text-[12px]">{pdf.size}</p>
                 </div>
-                <a> <AiFillDelete className="text-[35px] cursor-pointer hover:text-red-700  text-red-500" /></a>
+                <a onClick={() => deleteBook(pdf)}> <AiFillDelete className="text-[35px] cursor-pointer hover:text-red-700  text-red-500" /></a>
               </div>
              }
             )
