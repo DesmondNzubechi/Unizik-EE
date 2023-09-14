34a46030f5cb235e21522a3e6df980f857745d95
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-14 02:28:05 +0100

    new change

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -35,7 +35,7 @@ export const Login = () => {
     const navig = useNavigate();
     return (
         signedIn && mainUser[0]?.stats == 'user'? navig('/profile') :  signedIn && mainUser[0]?.stats == 'admin'? navig('/dashboard') : !signedIn &&
-        <div className="py-[70px] pt-[150px] ">
+        <div className="py-[70px] px-[40px] pt-[150px] ">
             <div className="flex flex-row justify-center">
             { spinnerJs &&  <div className="fixed bg-Tp w-full z-[500] left-0 right-0 flex justify-center h-full top-0 bottom-0 items-center"><BounceLoader color="#ffb700"
            size={100}
@@ -47,19 +47,19 @@ export const Login = () => {
                     <div className="flex flex-col gap-[10px] ">
                     <div className="flex flex-col ">
                 <label className="text-[20px] font-semibold" htmlFor="">Email</label>
-                <input onChange={(e) => setUserDetails({...userDetails, emailAddress: e.target.value})} value={userDetails.emailAddress} placeholder="Input email" className="border-yellow-500 bg-yellow-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="email" name="email" id="" />
+                <input onChange={(e) => setUserDetails({...userDetails, emailAddress: e.target.value})} value={userDetails.emailAddress} placeholder="Input email" className="shadow bg-transparent border-[2px]  rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="email" name="email" id="" />
             </div>
             <div  className="flex flex-col ">
                 <label className="text-[20px] font-semibold" htmlFor="">Password</label>
-                <input onChange={(e) => setUserDetails({...userDetails, password: e.target.value})} value={userDetails.password} className="border-yellow-500 bg-yellow-500 border-[2px] border text-white placeholder:text-white rounded outline-0 px-[20px] py-[5px] " type="password" placeholder="Input password" name="email" id="" />
+                <input onChange={(e) => setUserDetails({...userDetails, password: e.target.value})} value={userDetails.password} className=" shadow text-white placeholder:text-white rounded outline-0 px-[20px] py-[5px] " type="password" placeholder="Input password" name="email" id="" />
             </div>
-            <button onClick={signUserIn} className="bg-slate-900 py-[3px]  rounded shadow-2xl text-white text-[25px] font-semibold " type="button">login</button>
+            <button onClick={signUserIn} className="bg-slate-900 py-[3px]  hover:bg-slate-500 rounded shadow-2xl text-white text-[15px] md:text-[20px] font-semibold " type="button">login</button>
             <div className="flex flex-row items-center gap-2">
                 <span className="h-[4px] bg-yellow-500 w-full "></span>
                 <h1 className="uppercase font-bold">OR</h1>
                 <span  className="h-[4px] bg-yellow-500 w-full "></span>
             </div>
-           <p className="text-slate-800 font-[500] text-[20px] ">Don't have an account yet? <Link to='/signup' className="text-yellow-600">Sign Up</Link></p>
+           <p className="text-slate-800 font-[500] text-[12px] md:text-[15px] ">Don't have an account yet? <Link to='/signup' className="text-yellow-600">Sign Up</Link></p>
                     </div>
           
                 </form>
