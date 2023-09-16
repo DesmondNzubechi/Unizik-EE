640f387917f27df62564ef022e8410ca91c32323
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-16 03:08:21 +0100

    new change

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -47,22 +47,22 @@ export const Sign = () => {
             <div className="flex flex-row justify-center">
            { spinnerJs &&  <div className="fixed bg-Tp w-full z-[500] left-0 right-0 flex justify-center h-full top-0 bottom-0 items-center"><BounceLoader color="#ffb700"
            size={100}
-            /></div> }
-              {<form action=""  className="flex shadow-2xl my-5  p-5 rounded flex-col gap-[20px]">
-                    <div>
-                        <h1 className="uppercase  font-bold text-[30px] ">Signup here</h1>
-                    </div>
+                    /></div>}
+                     <div className="flex flex-row justify-center">
+             <div data-aos='zoom-in' className="bg-slate-900 shadow-2xl md:px-[70px]  flex flex-col  p-[20px] rounded shadow-2xl">
+                <h1 className="text-center text-slate-50 font-semibold text-[20px] uppercase font-myfont    mb-3">Sign up here</h1>
+              {<form action=""  className="flex  my-5  p-5 rounded flex-col gap-[20px]">
                     <div >
 
                     <div className="flex  flex-col gap-[10px] ">
 
 <div className="flex flex-col md:flex-row gap-5">
                     <div className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">First name:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">First name:</label>
                 <input onChange={(e) => setUserDetails({...userDetails, firstName: e.target.value})} value={userDetails.firstName} placeholder="Input First Name" className="border-yellow-500 bg-yellow-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="text" name="email" id="" />
             </div>
             <div className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Last name:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Last name:</label>
                 <input onChange={(e) => setUserDetails({...userDetails, lastName: e.target.value})} value={userDetails.lastName} placeholder="Input Last Name" className="border-yellow-500 bg-yellow-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="text" name="email" id="" />
             </div>
             
@@ -70,16 +70,16 @@ export const Sign = () => {
 
 <div className="flex flex-col md:flex-row gap-5">
                  { /*  <div className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Username:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Username:</label>
                 <input placeholder="Input Username" className="border-yellow-500 bg-yellow-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="text" name="email" id="" />
     </div>*/} 
             <div  className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Email Address:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Email Address:</label>
                 <input onChange={(e) => setUserDetails({...userDetails, emailAddress: e.target.value})} value={userDetails.emailAddress} className="border-yellow-500 bg-yellow-500 border-[2px] border text-white placeholder:text-white rounded outline-0 px-[20px] py-[5px] " type="email" placeholder="Input Email" name="email" id="" />
             </div>
 
             <div  className="flex w-full flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Level:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Level:</label>
                 <select onChange={(e) => setUserDetails({...userDetails, level: e.target.value})} value={userDetails.level} name="" id="" className="border-yellow-500 bg-yellow-500 border-[2px] border flex  flex-col gap-[20px] rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] ">
     <option className="text-[20px] " >select level</option>
     <option value="500l"  className="text-[20px] " >Aspirant</option>
@@ -95,28 +95,30 @@ export const Sign = () => {
                  </div>
                  <div className="flex flex-col md:flex-row gap-3">
             <div className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Password:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Password:</label>
                 <input onChange={(e) => setUserDetails({...userDetails, password: e.target.value})} value={userDetails.password} placeholder="Input Password" className="border-yellow-500 bg-yellow-500 border-[2px] border rounded text-white placeholder:text-white outline-0 px-[20px] py-[5px] " type="password" name="email" id="" />
             </div>
          
             
 
             <div  className="flex flex-col ">
-                <label className="text-[17px] font-[400]" htmlFor="">Confirm Password:</label>
+                <label className="text-[17px] text-slate-400 font-[400]" htmlFor="">Confirm Password:</label>
                 <input onChange={(e) => setUserDetails({...userDetails, confirmPassword: e.target.value})} value={userDetails.confirmPassword} className="border-yellow-500 bg-yellow-500 border-[2px] border text-white placeholder:text-white rounded outline-0 px-[20px] py-[5px] " type="password" placeholder="Confirm Password" name="email" id="" />
             </div>
             </div>
-            <button onClick={signUserUp} className="bg-slate-900 py-[3px] w-full  rounded shadow-2xl text-white text-[17px] font-[400] " type="button">Sign Up</button>
+            <button onClick={signUserUp} className="hover:bg-slate-50 border py-[3px] w-full  rounded shadow-2xl text-white text-[17px] text-slate-400 font-[400] " type="button">Sign Up</button>
             </div>
             <div className="flex flex-row items-center gap-2">
                 <span className="h-[4px] bg-yellow-500 w-full "></span>
-                <h1 className="uppercase font-[400] ">OR</h1>
+                <h1 className="uppercase text-slate-300 font-[400] ">OR</h1>
                 <span  className="h-[4px] bg-yellow-500 w-full "></span>
             </div>
-           <p className="text-slate-800 text-center font-[400] text-[15px] ">Already have an account? <Link to='/login' className="text-yellow-600">Log in</Link></p>
+           <p className="text-slate-300 text-center font-[400] text-[15px] ">Already have an account? <Link to='/login' className="text-yellow-600">Log in</Link></p>
                     </div>
           
                 </form>}
+                            </div>
+                            </div>
             </div>
         </div>
     )
