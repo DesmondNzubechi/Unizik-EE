2a1aab158a11347723615638197679064a44146b
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-09-19 14:56:22 +0100

    new change

--- src/components/ExStudentLeaders/ExStudentLeaders.jsx
+++ src/components/ExStudentLeaders/ExStudentLeaders.jsx
@@ -15,7 +15,7 @@ const ExStudentLeaders = [
         lga: 'Ngbo/Ohaukwu',
         position: 'Departmental President - 2020/2021',
         message: {
-            first: 'Dear Students and Future Leaders, As proud alumni of this esteemed institution, we stand before you as living testaments to the transformative power of education and resilience. We understand the challenges and opportunities that lie ahead, and we are here to offer guidance and support on your journey.',
+            first: 'Dear Students and Future Leaders, As proud alumni of this esteemed Department, we stand before you as living testaments to the transformative power of education and resilience. We understand the challenges and opportunities that lie ahead, and we are here to offer guidance and support on your journey.',
             second: 'Focus: A Key to Achieving Your Dreams. In your pursuit of knowledge and personal growth, remember that success often hinges on your ability to maintain unwavering focus. The world is brimming with distractions, and it is easy to lose sight of your goals. However, it is our shared belief that by concentrating your efforts, nurturing your passions, and sidestepping the allure of momentary distractions, you can achieve greatness. Embrace focus as your ally, and watch as it illuminates the path to your dreams.',
             third: 'Hard Work: The Foundation of Excellence. In the grand tapestry of life, hard work is the thread that binds success. It is through diligence, dedication, and the pursuit of excellence that we achieve remarkable feats. As alumni, we can attest that kindness and compassion also flow naturally from a heart dedicated to hard work. As you labor towards your aspirations, remember that every ounce of effort you invest contributes to your growth, your character, and the incredible future that awaits you.',
             fouth: 'Hope: The Eternal Light in Your Journey. In the face of adversity, uncertainty, and the challenges that may seem insurmountable, we encourage you to hold onto hope. It is hope that fuels our spirits when darkness looms, and it is hope that reminds us that even in the most trying moments, a brighter day awaits. Your efforts, late nights, and sacrifices are not in vain; they are stepping stones toward your goals. With hope as your guiding star, you will continue to evolve and eventually shine with the brilliance of your potential. Welcome the challenges that lie ahead, for they are opportunities in disguise. Embrace your education, harness your passion, and stay dedicated to your dreams. In unity and determination, you will carve a future filled with success and purpose.',
@@ -89,7 +89,7 @@ export const ExDeptExcos = () => {
                             </div>
     </div>
     <div className="max-w-[450px] flex-col gap-4  ">
-      <h1 className="font-semibold uppercase text-[20px] ">Message and encouragement to student</h1>
+      <h1 className="font-semibold uppercase text-[20px] ">Message and encouragement to our students</h1>
       <div className="text-slate-700 flex flex-col gap-3 py-[30px] ">
                                 <p className="md:text-[15px] text-[12px]" >{fullProfile.message?.first}</p>
                                 <p className="md:text-[15px] text-[12px]" >{fullProfile.message?.second}</p>
