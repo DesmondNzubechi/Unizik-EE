dbd25980cdb650650e8662f7cb760785bcf6bc8c
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-10-05 22:00:10 +0100

    new change

--- src/components/ProjectTeam/ProjectTeam.jsx
+++ src/components/ProjectTeam/ProjectTeam.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nzubechukwu from '../../assets/CRImages/B2R1.jpg';
 import dave from '../../assets/dave.jpg';
 import { AiOutlineMail, AiFillPhone } from 'react-icons/ai';
 import { IoSchoolSharp } from 'react-icons/io5';
 import { BiWorld } from 'react-icons/bi';
+import Aos from "aos";
 const ExStudentLeaders = [
     {
         name: 'Abugu Desmond Nzubechukwu',
@@ -11,7 +12,7 @@ const ExStudentLeaders = [
         number: '07084183611',
         picture: Nzubechukwu,
         level: '300l student',
-        position: 'Web Developer',
+        position: 'Web developer',
         link: 'https://www.nzubechukwu.com/',
         linkName: 'www.nzubechukwu.com'
     },
@@ -21,25 +22,28 @@ const ExStudentLeaders = [
         number: '09154064012',
         picture: dave,
         level: '300l student',
-        position: 'Web Developer',
-        link: '/',
+        position: 'web developer',
+        link: 'https://github.com/Davheed2',
         linkName: 'https://github.com/Davheed2'
     },
 ];
 
 export const ProjectTeam = () => {
+    useEffect(() => {
+        Aos.init();
+    })
     return (
         <div className="py-[100px] flex justify-center bg-slate-50 px-[20px]">
             <div>
                 <div>
-                    <div className="my-[30px]">
+                    <div data-aos='fade-up' aos-data-duration='2000' className="my-[30px]">
                     <h1 className="text-center uppercase text-[20px] md:text-[25px] font-bold  ">meet the team behind ee-unizik</h1>
                     <p className="text-center font-semibold text-slate-700 text-[15px] md:text-[20px]">Below are the team that built this website </p>
                     </div>
                     <div className="grid grid-cols-1 gap-[40px] md:grid-cols-2 lg:grid-cols-2">
                         {
                             ExStudentLeaders.map(info => {
-                                return <div className="shadow-xl w-fit   rounded-[10px]">
+                                return <div data-aos='fade-up' aos-data-duration='2000' className="shadow-xl w-fit   rounded-[10px]">
                                     <div className="max-w-[350px]">
                                     <img src={info.picture} className="rounded-[10px]" alt="" />
                                     </div> 
