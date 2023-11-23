5ae3012e2c60b80bba5af073c6db534ae1741de4
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-11-23 04:38:39 +0100

    added forgot password

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -10,6 +10,9 @@ import { fullNewsContext } from "../context/Context";
 import {AiOutlineMail} from 'react-icons/ai';
 import {RiLockPasswordFill} from 'react-icons/ri';
 import { Home } from "../Home/home";
+import { sendPasswordResetEmail } from "firebase/auth";
+import { HiXMark } from "react-icons/hi2";
+
 export const Login = () => {
     const navig = useNavigate();
     const { signedIn, mainUser,  } = useContext(fullNewsContext);
@@ -18,7 +21,48 @@ export const Login = () => {
         emailAddress: '',
         password: '',
     })
+
+    const [forgotPasswordEmail, setForgotPasswordEmail] = useState('')
     const [errorMessage, setErroMessage] = useState('');
+
+    const [form, setForm] = useState({
+        changePassword : 'top-[-2000px]', 
+        editProfile : 'top-[-2000px]',
+    });
+       //hide change password page
+       const hideChangePassword = () => {
+        setForm({
+           changePassword : 'top-[-1000px]',
+          editProfile : 'top-[-1000px]',
+         });
+    }
+     //view change password page
+     const viewChangePassword = () => {
+        setForm({
+           changePassword : 'top-0',
+          editProfile : 'top-[-2000px]',
+         })
+      }
+
+    const resetPassword = async () => {
+        if (forgotPasswordEmail === '') {
+            alert("Please input your email");
+            return;
+        }
+        const resetPass = window.confirm('Are you sure you want to reset your password??');
+        if (!resetPass) {
+          return;
+        }
+        try {
+          await sendPasswordResetEmail(auth, forgotPasswordEmail);
+          hideChangePassword();
+          alert('successful! Kindly check your email for reset password link');
+         
+        } catch (error) {
+          alert('Something went wrong❌ try again!')
+        }
+      }
+
     const signUserIn = async () => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (userDetails.emailAddress === '') {
@@ -53,7 +97,9 @@ export const Login = () => {
         return <Home />
     } else {
         return (
+           
             // signedIn? navig('/') : //&& mainUser[0]?.stats == 'user'? navig('/profile') :  signedIn && mainUser[0]?.stats == 'admin'? navig('/dashboard') : !signedIn &&
+            <>
             <div className="py-[150px] px-[20px]  ">
                 {spinnerJs && <div className="fixed bg-Tp w-full z-[500] left-0 right-0 flex justify-center h-full top-0 bottom-0 items-center"><BounceLoader color="#ffb700"
                     size={100}
@@ -79,12 +125,31 @@ export const Login = () => {
 
                                 }} value={userDetails.password} type="password" placeholder="******" className="p-3 rounded bg-slate-50 text-slate-900 outline-0 w-full shadow " />
                             </div>
-                            <button onClick={signUserIn} type="button" className="bg-yellow-500 hover:bg-yellow-700 text-slate-50 rounded text-[17px] font-semibold p-3">Login</button>
+                                <button onClick={signUserIn} type="button" className="bg-yellow-500 hover:bg-yellow-700 text-slate-50 rounded text-[17px] font-semibold p-3">Login</button>
+                                <button type="button" onClick={viewChangePassword} className="text-slate-200 hover:text-slate-500 text-[13px] font-300">Fogotten password?</button>
                             <p className="text-center text-[17px] text-slate-100 ">Don't have account yet? <Link to='/signup' className="text-yellow-500 text-[15px]">Sign Up</Link></p>
                         </form>
                     </div>
                 </div>
-            </div>
+                </div>
+                <span onClick={(e) => {
+              if (e.target.tagName === 'SPAN') {
+                setForm({ ...form, changePassword: 'top-[-2000px]' })
+              }
+            }} className={`fixed ${form.changePassword}   w-full h-full flex justify-center z-[500] p-[50px] bg-Tp`}>
+              <form action="" className="bg-yellow-500 overflow-y-auto relative rounded flex flex-col gap-4 p-[50px]">
+                <HiXMark onClick={hideChangePassword} className="absolute top-0 right-0 text-slate-50 active:text-slate-900 text-[25px] font-bold " />
+                <h1 className="text-[20px] font-bold uppercase text-slate-100 ">Change Password</h1>
+                <div className="flex flex-col">
+                  <label className="text-slate-900 font-[400] text-[20px]" htmlFor="email">Email:</label>
+                  <input onChange={(e) => setForgotPasswordEmail(e.target.value)} value={forgotPasswordEmail} type="email" className="border outline-0  p-2 rounded" placeholder="nzubechukwu1@gmail.com" />
+                </div>
+                <div className="flex flex-col">
+                  <button type="button" onClick={resetPassword} className="bg-slate-900 text-slate-50 p-2 my-[20px] rounded text-[20px] capitalize">Reset Password</button>
+                </div>
+              </form>
+            </span>
+                </>
         )
     }
 }
